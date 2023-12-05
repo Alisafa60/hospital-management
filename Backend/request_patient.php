3bde484693d7affc15c48750d2aa362f6a42cf5a
Alisafa60 <alisafa6100@gmail.com>
2023-12-05 03:29:29 +0200

    minor adjust in request emergency room

--- Backend/request_patient.php
+++ Backend/request_patient.php
@@ -9,12 +9,11 @@ $patient_id = $_POST['patient_id'];
 $admin_id = null; 
 $status = 'pending';
 
-// Insert the new emergency request into the emergency_requests table
 $insert_request_query = $mysqli->prepare('INSERT INTO emergency_requests (patient_id, admin_id, status) VALUES (?, ?, ?)');
 $insert_request_query->bind_param('iis', $patient_id, $admin_id, $status);
 
 if ($insert_request_query->execute()) {
-    $request_id = $mysqli->insert_id; // Get the generated request ID
+    $request_id = $mysqli->insert_id; 
 
     echo json_encode(["status" => "true", "message" => "Emergency request generated successfully", "request_id" => $request_id]);
 } else {
