e897821f4c0b1ef8567c7d69de7b863832bf04dd
Alisafa60 <alisafa6100@gmail.com>
2023-12-05 21:14:47 +0200

    create_admin adjustments and emergency request handling

--- Backend/create_admin.php
+++ Backend/create_admin.php
@@ -4,55 +4,37 @@ include("db_connection.php");
 
 $admin_username = $_POST['admin_username'];
 $admin_password = $_POST['admin_password'];
-//creating an admin to manage patients and users
-// check if admin already exists
-$check_query = $mysqli->prepare('SELECT user_id FROM users WHERE username=? AND role="admin"');
-$check_query->bind_param('s', $admin_username);
-$check_query->execute();
-$check_query->store_result();
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
 
-if ($check_query->num_rows > 0) {
-    $response = ["status" => "false", "message" => "Admin already exists"];
+// Check if admin already exists
+// Hash the admin password
+$hashed_admin_password = password_hash($admin_password, PASSWORD_DEFAULT);
+
+// Insert admin user into the database
+$insert_query = $mysqli->prepare('INSERT INTO users(first_name, last_name, username, password, role, approved) VALUES (?, ?, ?, ?, "admin", 1)');
+$insert_query->bind_param('ssss', $first_name, $last_name, $admin_username, $hashed_admin_password);
+$insert_query->execute();
+
+if ($insert_query->affected_rows > 0) {
+    // Issue a JWT for the newly created admin
+    $admin_user_id = $insert_query->insert_id;
+    $token_payload = [
+        "user_id" => $admin_user_id,
+        "username" => $admin_username,
+        "role" => "admin"
+    ];
+
+    $secret_key = "lazy_susan";
+    $jwt_token = jwt_encode($token_payload, $secret_key);
+
+    $response = ["status" => "true", "token" => $jwt_token];
 } else {
-    $hashed_admin_password = password_hash($admin_password, PASSWORD_DEFAULT);
-
-    $insert_user_query = $mysqli->prepare('INSERT INTO users(username, password, role, approved) VALUES (?, ?, "admin", 1)');
-    $insert_user_query->bind_param('ss', $admin_username, $hashed_admin_password);
-    $insert_user_query->execute();
-
-    if ($insert_user_query->affected_rows > 0) {
-        $admin_user_id = $insert_user_query->insert_id;
-
-        $insert_admin_query = $mysqli->prepare('INSERT INTO admins(user_id, first_name, last_name) VALUES (?, ?, ?)');
-        $insert_admin_query->bind_param('iss', $admin_user_id, $first_name, $last_name);
-        
-        $first_name = $_POST['first_name'];
-        $last_name = $_POST['last_name'];
-
-        $insert_admin_query->execute();
-
-        if ($insert_admin_query->affected_rows > 0) {
-            $token_payload = [
-                "user_id" => $admin_user_id,
-                "username" => $admin_username,
-                "role" => "admin"
-            ];
-
-            $secret_key = "lazy_susan";
-            $jwt_token = jwt_encode($token_payload, $secret_key);
-
-            $response = ["status" => "true", "token" => $jwt_token];
-        } else {
-            $response = ["status" => "false", "message" => "Failed to create admin details"];
-        }
-
-        $insert_admin_query->close();
-    } else {
-        $response = ["status" => "false", "message" => "Failed to create admin"];
-    }
-
-    $insert_user_query->close();
+    $response = ["status" => "false", "message" => "Failed to create admin"];
 }
+
+
+echo json_encode($response);
 
 function jwt_encode($payload, $secret_key) {
     // Base64Url encode the JWT header and payload
@@ -75,7 +57,5 @@ function base64UrlEncode($data) {
     $base64Url = strtr($base64, '+/', '-_');
     return rtrim($base64Url, '=');
 }
-
-echo json_encode($response);
 
 
---
+++ Backend/emergency_patient.php
@@ -0,0 +1,34 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+include("db_connection.php");
+include("meddleware.php");
+
+authorize('patient');
+
+$user_id = $_POST['user_id'];
+
+$user_query = $mysqli->prepare('SELECT first_name, last_name FROM users WHERE user_id = ?');
+$user_query->bind_param('i', $user_id);
+$user_query->execute();
+$user_query->bind_result($first_name, $last_name);
+
+if ($user_query->fetch()) {
+    $user_query->close();  
+    $insert_request_query = $mysqli->prepare('INSERT INTO emergencyroomrequest(user_id, first_name, last_name, status) VALUES (?, ?, ?, "pending")');
+    $insert_request_query->bind_param('iss', $user_id, $first_name, $last_name);
+
+    if ($insert_request_query->execute()) {
+        $request_id = $mysqli->insert_id;
+
+        echo json_encode(["status" => "true", "message" => "Emergency request generated successfully", "request_id" => $request_id]);
+    } else {
+        echo json_encode(["status" => "false", "message" => "Failed to generate emergency request: " . $mysqli->error]);
+    }
+
+    $insert_request_query->close();
+} else {
+    echo json_encode(["status" => "false", "message" => "User not found"]);
+}
+
+$mysqli->close();
+?>
